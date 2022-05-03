0debafbda8e8076ee42b8ab1f4e0f3ca02970898
KryozAlias <slippinice@gmail.com>
2022-05-03 19:40:36 -0400

    Added backend functionality.

--- C#/WPFD2/WPFD2/APIManager.cs
+++ C#/WPFD2/WPFD2/APIManager.cs
@@ -11,35 +11,30 @@ using BungieSharper.Entities.Destiny.Entities.Characters;
 using BungieSharper.Entities.Destiny.Entities.Inventory;
 using BungieSharper.Entities.Destiny.Entities.Items;
 using BungieSharper.Entities.Destiny.Responses;
+using BungieSharper.Entities.User;
 
 namespace WPFD2
 {
     public class APIManager
     {
         String apiKey = "7c1528dadd144643b93a7ceb2fff5685";
-        BungieSharper.Client.BungieApiClient client;
+        BungieSharper.Client.BungieApiClient Client;
         BungieSharper.Entities.TokenResponse Token;
-        long d2memId;
-        string userName;
+        DestinyLinkedProfilesResponse _UserProfile;
+        DestinyProfileUserInfoCard _DestinyProfile;
+        List<DestinyCharacterComponent> _CharacterList;
+        UserInfoCard _BnetProfile;
         public APIManager()
         {
             BungieSharper.Client.BungieClientConfig config = new BungieSharper.Client.BungieClientConfig();
             config.OAuthClientId = 39661;
             config.OAuthClientSecret = "zGpr-mMHVnYhIYJODAlphLzvsiQQ4HfgyFjrZU2UVvE";
             config.ApiKey = apiKey;
-            client = new BungieSharper.Client.BungieApiClient(config);
-        }
-        public long getD2MemID()
-        {
-            return d2memId;
-        }
-        public string getUserName()
-        {
-            return userName;
+            Client = new BungieSharper.Client.BungieApiClient(config);
         }
         public void Authenticate() 
         {
-            string url = client.OAuth.GetOAuthAuthorizationUrl();
+            string url = Client.OAuth.GetOAuthAuthorizationUrl();
             var psi = new ProcessStartInfo
             {
                 FileName = url,
@@ -49,21 +44,32 @@ namespace WPFD2
         }
         public string getAccessToken(string authToken)
         {
-            BungieSharper.Entities.TokenResponse token = client.OAuth.GetOAuthToken(authToken).Result;
+            BungieSharper.Entities.TokenResponse token = Client.OAuth.GetOAuthToken(authToken).Result;
             if(token.AccessToken == null)
             {
                 return token.ErrorDescription;
             }
             Token = token;
-            profile();
-            
+            _UserProfile = Client.Api.Destiny2_GetLinkedProfiles((long)Token.MembershipId, BungieSharper.Entities.BungieMembershipType.TigerSteam).Result;
+            IEnumerable<DestinyProfileUserInfoCard> profileEnumerator = _UserProfile.Profiles;
+            foreach (DestinyProfileUserInfoCard profile in profileEnumerator)
+            {
+                if (profile.MembershipType == BungieSharper.Entities.BungieMembershipType.TigerSteam)
+                {
+                    _DestinyProfile = profile;
+                }
+            }
             return "True";
         }
-        public string profile()
+        private void GetCharacter()
+        {
+
+        }
+/*        public string profile()
         {
 
             BungieSharper.Entities.Destiny.Responses.DestinyLinkedProfilesResponse resp =
-                client.Api.Destiny2_GetLinkedProfiles((long)Token.MembershipId, BungieSharper.Entities.BungieMembershipType.TigerSteam).Result;
+                
             IEnumerable<DestinyProfileUserInfoCard> profileEnumerator = resp.Profiles;
             foreach(DestinyProfileUserInfoCard profile in profileEnumerator)
             {
@@ -76,20 +82,18 @@ namespace WPFD2
             }
             return "No Profile found";
 
-        }
+        }*/
 
-        public string getInventory() {
+        public string GetInventory() {
             List<DestinyComponentType> components = new List<DestinyComponentType>();
             components.Add(DestinyComponentType.Characters);
-      
-            DestinyProfileResponse resp = client.Api.Destiny2_GetProfile(d2memId, BungieSharper.Entities.BungieMembershipType.TigerSteam, components).Result;
+            DestinyProfileResponse resp = Client.Api.Destiny2_GetProfile(_DestinyProfile.MembershipId, BungieSharper.Entities.BungieMembershipType.TigerSteam, components).Result;
             DictionaryComponentResponseOfint64AndDestinyCharacterComponent a = resp.Characters;
             Dictionary<long, DestinyCharacterComponent> characterDic = a.Data;
-
             List<long> idList = new List<long>(characterDic.Keys);
             components = new List<DestinyComponentType>();
             components.Add(DestinyComponentType.CharacterEquipment);
-            DestinyCharacterResponse resp2 = client.Api.Destiny2_GetCharacter(idList[0], d2memId, BungieSharper.Entities.BungieMembershipType.TigerSteam, components).Result;
+            DestinyCharacterResponse resp2 = Client.Api.Destiny2_GetCharacter(idList[0], _DestinyProfile.MembershipId, BungieSharper.Entities.BungieMembershipType.TigerSteam, components).Result;
             SingleComponentResponseOfDestinyInventoryComponent d = resp2.Equipment;
             DestinyInventoryComponent f = d.Data;
             IEnumerable<DestinyItemComponent> g = f.Items;
