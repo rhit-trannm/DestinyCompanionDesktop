d74874396142b025734a38912eb2073fce2e44c4
hendermd <hendermd@rose-hulman.edu>
2022-05-19 16:56:18 -0400

    ManifestsSeperated

--- C#/WPFD2/WPFD2/DevPage.xaml.cs
+++ C#/WPFD2/WPFD2/DevPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +29,7 @@ namespace WPFD2
             InitializeComponent();
 
             textBlock.Text = this.manager.getSQLManager().initiateUser(1);
-           
+
         }
 
         private void Update(object sender, RoutedEventArgs e)
@@ -45,14 +48,120 @@ namespace WPFD2
 
         }
 
+
         private void id_read_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
 
+        private void updateBucketManifest()
+        {
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\Outputs\DestinyInventoryBucketDefinition.json");
+            string path = System.IO.Path.GetFullPath(sFile);
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                JObject j = new JObject();
+                JObject items = JsonConvert.DeserializeObject<JObject>(json);
+                dynamic k = items;
+                foreach (var item in items)
+                {
+                    if (items.SelectToken($"{item.Key}.displayProperties.name") != null)
+                    {
+                        try
+                        {
+                            this.manager.getAPIManager().updateBucketManifest(long.Parse(item.Key),
+                                                        items.SelectToken($"{item.Key}.displayProperties.name").ToString());
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void updateItemManifest()
+        {
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\Outputs\DestinyInventoryItemDefinition.json");
+            string path = System.IO.Path.GetFullPath(sFile);
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                JObject j = new JObject();
+                JObject items = JsonConvert.DeserializeObject<JObject>(json);
+                dynamic k = items;
+                foreach (var item in items)
+                {
+                    if (items.SelectToken($"{item.Key}.displayProperties.name") != null)
+                    {
+                        try
+                        {
+                            this.manager.getAPIManager().updateManifest(
+                                                                    long.Parse(item.Key),
+                                                                    long.Parse(items.SelectToken($"{item.Key}.inventory.bucketTypeHash").ToString()),
+                                                                    items.SelectToken($"{item.Key}.displayProperties.name").ToString(),
+                                                                    items.SelectToken($"{item.Key}.displayProperties.description").ToString(),
+                                                                    items.SelectToken($"{item.Key}.inventory.tierTypeName").ToString(),
+                                                                    long.Parse(items.SelectToken($"{item.Key}.itemCategoryHashes[0]").ToString()),
+                                                                    items.SelectToken($"{item.Key}.displayProperties.icon").ToString(),
+                                                                    long.Parse(items.SelectToken($"{item.Key}.itemCategoryHashes[1]").ToString()),
+                                                                    long.Parse(items.SelectToken($"{item.Key}.itemCategoryHashes[2]").ToString()));
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void updateCategoryManifest()
+        {
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\Outputs\DestinyItemCategoryDefinition.json");
+            string path = System.IO.Path.GetFullPath(sFile);
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                JObject j = new JObject();
+                JObject items = JsonConvert.DeserializeObject<JObject>(json);
+                dynamic k = items;
+                foreach (var item in items)
+                {
+                    if (items.SelectToken($"{item.Key}.displayProperties.name") != null)
+                    {
+                        try
+                        {
+                            this.manager.getSQLManager().AddCategoryDefinition(
+                                                                    long.Parse(item.Key),
+                                                                    items.SelectToken($"{item.Key}.displayProperties.name").ToString());
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+
+                    }
+
+                }
+            }
+        }
+
         private void updateManifest_Click(object sender, RoutedEventArgs e)
         {
-
+            updateBucketManifest();
+            updateItemManifest();
+            updateCategoryManifest();
+            //Console.Write(items);
+            /*			foreach (BungieSharper.Entities.Destiny.Entities.Items.DestinyItemComponent element in items)
+                        {
+                            Console.Write($"{element.ItemHash} \n");
+                        }*/
         }
     }
 }
